3c6e45242d298cda26045fc1c27ce4351a7aee43
cutefcc <cutefcc@126.com>
2022-09-27 23:24:33 +0800

    feat: DeepReadonly

--- 00009-medium-deep-readonly.ts
+++ 00009-medium-deep-readonly.ts
@@ -51,9 +51,31 @@ type Expected = {
 
 // ============= Your Code Here =============
 // deep 就想到 递归，遇到对象和数组就递归，但是没想出解决方案
-// 👇的方案很巧妙，通过 keyof T extends never 来判断是否是对象，如果是对象就递归，如果不是就直接返回
+// 👇的方案 keyof T extends never  bad case
 type DeepReadonly<T> = keyof T extends never
   ? T
   : {
       readonly [M in keyof T]: DeepReadonly<T[M]>;
     };
+
+// ============= Test Cases =============
+// keyof T extends never 并不好
+type Hmm<T> = keyof T extends never ? true : false;
+type X1 = Hmm<{ a: string }>; // false, "a" is a known key
+type X2 = Hmm<{}>; // true, there are no known keys
+type X3 = Hmm<object>; // true, there are no known keys
+type X4 = Hmm<string>; // false, there are keys like "toUpperCase"
+type X5 = Hmm<{ a: string } | { b: string }>; // true, unions with no common keys have no known keys
+
+type Z = DeepReadonly<{ a: string } | { b: string }>;
+// type Z = {a: string} | {b: string}  OOPS
+
+declare const z: Z;
+if ("a" in z) {
+  z.a = ""; // no error, not readonly
+}
+// The "right" definition of DeepReadonly<T> is probably just
+type DeepReadonly2<T> = { readonly [K in keyof T]: DeepReadonly2<T[K]> };
+type Z1 = DeepReadonly2<{ a: string } | { b: string }>;
+type Z2 = DeepReadonly2<{ a: string }>;
+// ============= Test Cases =============

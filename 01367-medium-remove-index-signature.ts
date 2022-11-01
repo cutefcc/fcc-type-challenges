fea6290bb927ddd97b53b950ec0ba00d87df1694
cutefcc <cutefcc@126.com>
2022-11-01 09:15:05 +0800

    feat: RemoveIndexSignature

---
+++ 01367-medium-remove-index-signature.ts
@@ -0,0 +1,44 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type Foo = {
+  [key: string]: any;
+  foo(): void;
+};
+
+type Bar = {
+  [key: number]: any;
+  bar(): void;
+  0: string;
+};
+
+const foobar = Symbol("foobar");
+type FooBar = {
+  [key: symbol]: any;
+  [foobar](): void;
+};
+
+type Baz = {
+  bar(): void;
+  baz: string;
+};
+
+type cases = [
+  Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
+  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void; 0: string }>>,
+  Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
+  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void; baz: string }>>
+];
+
+// 判断 Signature
+type IsSignature<T> = string extends T
+  ? never
+  : number extends T
+  ? never
+  : symbol extends T
+  ? never
+  : T;
+// ============= Your Code Here =============
+type RemoveIndexSignature<T extends Record<string, any>> = {
+  [M in keyof T as IsSignature<M> extends never ? never : M]: T[M];
+};

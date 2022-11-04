7e0f85dd5a48ba84b598e9ce2cdc5e7375595745
cutefcc <cutefcc@126.com>
2022-11-04 12:49:37 +0800

    feat: MyReturnType

--- 00002-medium-return-type.ts
+++ 00002-medium-return-type.ts
@@ -19,9 +19,15 @@ type ComplexObject = {
 
 const fn = (v: boolean) => (v ? 1 : 2);
 const fn1 = (v: boolean, w: any) => (v ? 1 : 2);
+// 第二遍 很快写出来了
+type MyReturnType<T extends (...params: any[]) => any> = T extends (
+  ...params: any[]
+) => infer M
+  ? M
+  : never;
 
 // ============= Your Code Here =============
 // 一开始能写出下面的代码， 发现后面两个用例通不过，原因：如果我们传递一个带参数的函数，它将无法分配给我们的类型 () => infer R。
 // type MyReturnType<T> = T extends () => infer R ? R : never;
-// 改进后的，能接受任何参数的
-type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+// // 改进后的，能接受任何参数的
+// type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
